e4c5a74c7cdad6434539c264f87c5f824ca199a3
Erick Mora <m.erickpeligro@gmail.com>
2024-11-13 21:20:08 -0600

    avance desde mi casa a proyecto de instagram

--- projects/00-repaso/src/InstagramFollowCard.jsx
+++ projects/00-repaso/src/InstagramFollowCard.jsx
@@ -8,18 +8,19 @@ export function InstagramFollowCard (){
     return(
         <article className="ig-followCard">
             <header className="ig-followCard-header">
-                <img className="ig-followCard-avatar"
+                <img 
+                className="ig-followCard-avatar"
                 alt="Foto de perfil" 
                 src ="https://unavatar.io/youtube/septarianlyrics8704" 
                 />
-                <div>
-                    <strong>Erick Mora</strong>
-                    <span>septarian_9</span>
+                <div className="ig-followCard-info">
+                    <strong >Erick Mora</strong>
+                    <span className="ig-followCard-infoUserName">septarian_9</span>
                 </div>
             </header>
 
             <aside>
-                <button>
+                <button className="ig-followCard-button">
                     Seguir
                 </button>
             </aside>
