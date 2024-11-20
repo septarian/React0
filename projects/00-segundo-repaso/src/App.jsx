71b641d0e701c7920e97d3c150e8d134360fa600
Erick Mora <m.erickpeligrogmail.com>
2024-11-20 17:08:38 -0600

    Inicio de proyecto repaso de youtube

---
+++ projects/00-segundo-repaso/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+export function App(){
+  return (
+    <section className='yt-suscriptionsCard'>
+      <header className='yt-suscriptionsCard-header'>
+        <img className='yt-suscriptionCard-img' src='https://unavatar.io/youtube/disneyxd' />
+          <div className='yt-suscriptionCard-text'>
+            <span>Disney XD</span>
+          </div>
+      </header>
+
+
+    <aside>
+      <icon>
+
+      </icon>
+    </aside>
+  </section>
+  )
+}
